f8037dd46388df23418899a8d1613b84b77e0d58
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2025-06-24 00:33:36 +0530

    Update TwoSum.cpp

--- Cpp/Day1/TwoSum.cpp
+++ Cpp/Day1/TwoSum.cpp
@@ -1,1 +1,17 @@
-2sum.cpp
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& a, int t) 
+    {
+        unordered_map<int,int>mp;
+        for(int i=0;i<a.size();i++)
+        {
+            int k=t-a[i];
+            if(mp.find(k)!=mp.end())
+            {
+                return {i,mp[k]};
+            }
+            mp[a[i]]=i;
+        }
+        return {};
+    }
+};
