961f3651fbe860f2d39f43669b4561f79642b7a0
Sulagna Dutta Choudhuri <83762319+Sulagna02@users.noreply.github.com>
2025-09-08 06:33:51 +0530

    Create BestTimetoBuyandSellStock.java

---
+++ Java/Day10-08.09.2025/BestTimetoBuyandSellStock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) 
+    {
+        int min=prices[0], profit=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            min=Math.min(prices[i],min);
+            profit=Math.max(prices[i]-min,profit);
+        }
+        return profit;
+
+        
+    }
+}
