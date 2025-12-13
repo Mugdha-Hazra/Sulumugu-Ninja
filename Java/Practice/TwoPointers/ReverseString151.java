fa923f6be602b63c0867ea9b4ecc20f7e57c4048
Sulagna Dutta Choudhuri <83762319+Sulagna02@users.noreply.github.com>
2025-12-13 19:11:22 +0530

    Implement reverseWords method to reverse string words

---
+++ Java/Practice/TwoPointers/ReverseString151.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String reverseWords(String s) 
+    {
+        s=s.trim();
+        String[] arr=s.split("\\s+");
+
+        int i=0, j=arr.length-1;
+        while(i<j)
+        {
+            String temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        return String.join(" ",arr);
+        
+    }
+}
