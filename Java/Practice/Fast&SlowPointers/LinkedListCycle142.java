9bf879b602bf473efaac0b7a88e88836bd5e7d50
Sulagna Dutta Choudhuri <83762319+Sulagna02@users.noreply.github.com>
2025-12-18 17:23:27 +0530

    Create LinkedListCycle142.java

---
+++ Java/Practice/Fast&SlowPointers/LinkedListCycle142.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head)
+    {
+        ListNode fast=head, slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast)
+            {
+                break;
+            }
+        }
+        if(fast==null || fast.next==null)
+        return null;
+
+        ListNode n1=slow, n2=head;
+        while(n1!=n2)
+        {
+            n1=n1.next;
+            n2=n2.next;
+        }
+        return n1;
+        
+    }
+}
