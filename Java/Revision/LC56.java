cf40b4ac96f7efad2b024208e9edec2fcca263f6
Sulagna Dutta Choudhuri <83762319+Sulagna02@users.noreply.github.com>
2025-12-23 21:08:04 +0530

    Create LC56.java

---
+++ Java/Revision/LC56.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] merge(int[][] intervals)
+    {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        List<int[]> res=new ArrayList<>();
+        res.add(intervals[0]);
+        for(int i=1;i<intervals.length;i++)
+        {
+            int[] last=res.get(res.size()-1);
+            int[] curr=intervals[i];
+
+            if(curr[1]>=last[0] && last[1]>=curr[0])
+            {
+                last[0]=Math.min(last[0],curr[0]);
+                last[1]=Math.max(last[1],curr[1]);
+                res.set(res.size()-1,last);
+            }
+            else
+            res.add(curr);
+            
+        }
+        return res.toArray(new int[res.size()-1][2]);
+    }
+}
